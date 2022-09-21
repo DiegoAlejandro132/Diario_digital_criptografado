10ecbf6466439fc652c78f533451f758cde23fac
DiegoAlejandro132 <diegoalejandro132@gmail.com>
2022-09-21 14:59:37 -0300

    criação da listagem de pacientes, acesso do psicologo na agenda e acesso a algumas informações do formulario do paciente selecionado, criação de classe para inserir metodos de validação

--- app/src/main/java/tcc/com/diario_digital_criptografado/AgendaUsuarioActivity.kt
+++ app/src/main/java/tcc/com/diario_digital_criptografado/AgendaUsuarioActivity.kt
@@ -3,20 +3,30 @@ package tcc.com.diario_digital_criptografado
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_agenda_usuario.*
+import kotlinx.android.synthetic.main.activity_formulario_diario.*
+import tcc.com.diario_digital_criptografado.util.AuthUtil
+import tcc.com.diario_digital_criptografado.util.Validation
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 class AgendaUsuarioActivity : AppCompatActivity() {
-
-    @RequiresApi(Build.VERSION_CODES.O)
+    private lateinit var database : DatabaseReference
+    private lateinit var emailUsuarioSelecionado : String
+    private lateinit var tipoUsuario : String
     override fun onCreate(savedInstanceState: Bundle?) {
+        verifyUser()
+        Log.d("msg", "1")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agenda_usuario)
 
@@ -29,51 +39,49 @@ class AgendaUsuarioActivity : AppCompatActivity() {
             var dia = i3
             val data = "${dia}-${mes}-${ano}"
 
-            var formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+            var formatter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                DateTimeFormatter.ofPattern("dd-MM-yyyy")
+            } else {
+                TODO("VERSION.SDK_INT < O")
+            }
+
             val diaAtual = LocalDateTime.now()
             val dataFormatada = diaAtual.format(formatter)
-            Toast.makeText(this@AgendaUsuarioActivity, "agenda : ${dataFormatada}", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, FormularioDiarioActivity::class.java)
-            intent.putExtra("dataSelecionada", data)
-            startActivity(intent)
+
+            val intentEmail = intent
+            emailUsuarioSelecionado = if(tipoUsuario == "Psicólogo"){
+                intentEmail.getStringExtra("email").toString()
+            }else ""
+
+            if(Validation.validateDateCalendar(data, dataFormatada)){
+                val intent = Intent(this, FormularioDiarioActivity::class.java)
+                intent.putExtra("dataSelecionada", data)
+                if(tipoUsuario == "Psicólogo")
+                    intent.putExtra("emailUsuarioSelecionado", emailUsuarioSelecionado)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this@AgendaUsuarioActivity, "Data futura não é permitida", Toast.LENGTH_SHORT).show()
+            }
             }
         }
 
+    // <---------------------------------------------------- funções ----------------------------------------------------->
+    // <---------------------------------------------------- funções ----------------------------------------------------->
+    // <---------------------------------------------------- funções ----------------------------------------------------->
+    // <---------------------------------------------------- funções ----------------------------------------------------->
+    // <---------------------------------------------------- funções ----------------------------------------------------->
+    // <---------------------------------------------------- funções ----------------------------------------------------->
+
+    private fun verifyUser(){
+        database = FirebaseDatabase.getInstance().getReference("users")
+        database.child(AuthUtil.getCurrentUser()!!).get().addOnSuccessListener {
+            tipoUsuario = it.child("tipo_perfil").value.toString()
+            if(tipoUsuario == "Psicólogo"){
+                val intent = intent
+                emailUsuarioSelecionado = intent.getStringExtra("email").toString()
+            }else if (tipoUsuario == "Usuário do diário"){
+                emailUsuarioSelecionado = ""
+            }
+        }
     }
-
-    /*private fun validateDate(data : String, dataAtual : String) : Boolean{
-        var dia : Int
-        var mes : Int
-        var ano : Int
-        if (data[2].equals("-")){
-            dia = "${data[1]}".toInt()
-            if(data[3].equals("-")){
-                mes = "${data[2]}".toInt()
-                ano = "${data[4]}${data[5]}${data[6]}${data[7]}".toInt()
-            }else{
-                mes = "${data[2]}${data[3]}".toInt()
-                ano = "${data[5]}${data[6]}${data[7]}${data[8]}".toInt()
-            }
-        }else{
-            dia = "${data[0]}${data[1]}".toInt()
-            if(data[4].equals("-")){
-                mes = "${data[3]}".toInt()
-                ano = "${data[5]}${data[6]}${data[7]}${data[8]}".toInt()
-            }else{
-                mes = "${data[3]}${data[4]}".toInt()
-                ano = "${data[6]}${data[7]}${data[8]}${data[9]}".toInt()
-            }
-        }
-        Toast.makeText(this@AgendaUsuarioActivity, "comemorar", Toast.LENGTH_LONG).show()
-        val total = dia + (mes * 30)  + (ano * 365)
-
-        val diaAtual = "${dataAtual[0]}${dataAtual[1]}".toInt()
-        val mesAtual = "${dataAtual[3]}${dataAtual[4]}".toInt()
-        val anoAtual = "${dataAtual[6]}${dataAtual[7]}${dataAtual[8]}${dataAtual[9]}".toInt()
-        val totalAtual = diaAtual + (mesAtual * 30) + (anoAtual * 365)
-
-        if(total <= totalAtual){
-            return true
-        }
-        return false
-    }*/
+}
--- app/src/main/java/tcc/com/diario_digital_criptografado/util/Validation.kt
+++ app/src/main/java/tcc/com/diario_digital_criptografado/util/Validation.kt
@@ -2,7 +2,43 @@ package tcc.com.diario_digital_criptografado.util
 
 class Validation {
 
-    fun validateDayOnCalendar(){
+    companion object {
+        fun validateDateCalendar(data : String, dataAtual : String) : Boolean{
+            var dia : Int
+            var mes : Int
+            var ano : Int
+            if (data[1].toString() == "-"){
+                dia = "${data[0]}".toInt()
+                if(data[3].toString() == "-"){
+                    mes = "${data[2]}".toInt()
+                    ano = "${data[4]}${data[5]}${data[6]}${data[7]}".toInt()
+                }else{
+                    mes = "${data[2]}${data[3]}".toInt()
+                    ano = "${data[5]}${data[6]}${data[7]}${data[8]}".toInt()
+                }
+            }else{
+                dia = "${data[0]}${data[1]}".toInt()
+                if(data[4].toString() == "-" ){
+                    mes = "${data[3]}".toInt()
+                    ano = "${data[5]}${data[6]}${data[7]}${data[8]}".toInt()
+                }else{
+                    mes = "${data[3]}${data[4]}".toInt()
+                    ano = "${data[6]}${data[7]}${data[8]}${data[9]}".toInt()
+                }
+            }
+            val total = dia + (mes * 30)  + (ano * 365)
 
+            val diaAtual = "${dataAtual[0]}${dataAtual[1]}".toInt()
+            val mesAtual = "${dataAtual[3]}${dataAtual[4]}".toInt()
+            val anoAtual = "${dataAtual[6]}${dataAtual[7]}${dataAtual[8]}${dataAtual[9]}".toInt()
+            val totalAtual = diaAtual + (mesAtual * 30) + (anoAtual * 365)
+
+            if(total <= totalAtual){
+                return true
+            }
+            return false
+        }
     }
+
+
 }

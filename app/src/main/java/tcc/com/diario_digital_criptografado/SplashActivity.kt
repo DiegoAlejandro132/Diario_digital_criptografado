8bafd3291d525597ee0b0ef9e0cc7cf87887f067
Terceros <diego.terceros@ikhon.com.br>
2022-10-13 23:55:21 -0400

    criar splash
    -criação de tela splash e e definir ela como main
    -contador na tela de splash antes de iniciar função

---
+++ app/src/main/java/tcc/com/diario_digital_criptografado/SplashActivity.kt
@@ -0,0 +1,52 @@
+package tcc.com.diario_digital_criptografado
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import com.google.firebase.database.FirebaseDatabase
+import tcc.com.diario_digital_criptografado.psicologoActivities.ListagemPacientesActivity
+import tcc.com.diario_digital_criptografado.usuarioActivities.AgendaUsuarioActivity
+import tcc.com.diario_digital_criptografado.util.AuthUtil
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //contador para mostrar splash antes de iniciar tarefa
+        object : CountDownTimer(1500, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                usuarioEstaLogado()
+            }
+        }.start()
+
+
+    }
+
+    private fun usuarioEstaLogado(){
+        var database = FirebaseDatabase.getInstance().getReference("users")
+        if(AuthUtil.getCurrentUser() !== null){
+            database.get().addOnSuccessListener {
+                if(it.child("${AuthUtil.getCurrentUser()}/tipo_perfil").value.toString() == "Usuário do diário"){
+                    val intent = Intent(this, AgendaUsuarioActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else if(it.child("${AuthUtil.getCurrentUser()}/tipo_perfil").value.toString() == "Psicólogo"){
+                    val intent = Intent(this, ListagemPacientesActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }else{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
